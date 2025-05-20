10c0736a517f79bd4a5bfacfe573fd84bd90f540
Rodrigo Araujo <rasch.dev@gmail.com>
2025-05-20 07:07:01 -0300

    Project created

---
+++ src/main/java/UML/springUML/SpringUmlApplication.java
@@ -0,0 +1,13 @@
+package UML.springUML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UML/springUML/SpringUmlApplicationTests.java
@@ -0,0 +1,13 @@
+package UML.springUML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
